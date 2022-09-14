4d4e0bf225f0285efd9cb6c7fd5308ef53b2adcc
hmeleiros <hmeleiros@gmail.com>
2022-09-14 16:21:32 +0200

    Avances tutorial

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 // import { TaskCard } from "./Task";
 // import { Button } from "./Button";
@@ -9,16 +9,27 @@ import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Counter() {
-  const [ mensaje, setMensaje] = useState('');
+  const [mensaje, setMensaje] = useState("");
+  const [counter, setCounter] = useState(0);
+
+  useEffect(function () {
+    console.log("render");
+  }, [counter]);
 
   return (
     <div>
-    <input onChange={e => setMensaje(e.target.value)}/>
-    <button onClick={() => {
-      alert("El mensaje es: " + mensaje)
-    }}>
-      Save
-    </button>
+      <input onChange={(e) => setMensaje(e.target.value)} />
+      <button
+        onClick={() => {
+          alert("El mensaje es: " + mensaje);
+        }}
+      >
+        Save
+      </button>
+
+      <hr />
+      <h1>Counter: {counter} </h1>
+      <button onClick={() => setCounter(counter + 1)}>Incrementar</button>
     </div>
   );
 }
